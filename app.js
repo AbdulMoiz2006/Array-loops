0e405e217de87f739e697f01fb0a236c1083fade
AbdulMoiz2006 <156921517+AbdulMoiz2006@users.noreply.github.com>
2024-10-25 02:57:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,119 @@
+// var fruits = ["Mango","Apple","Banana","Orange",]
+
+// fruits.push("Graphs");
+
+// console.log(fruits);
+
+
+
+// var fruits = ["Mango","Apple","Banana","Orange",]
+
+// fruits.pop();
+
+// console.log(fruits);
+
+
+
+// var fruits = ["Mango","Apple","Banana","Orange",]
+
+// fruits.shift();
+
+// console.log(fruits);
+
+
+
+
+
+// var fruits = ["Mango","Apple","Banana","Orange",]
+
+// fruits.unshift("Graphs");
+ 
+// console.log(fruits);
+
+
+
+// var fruits = ["Mango","Apple","Banana","Orange",]
+
+// fruits[1] = "Strawberry"
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+// var fruits = ["Mango","Apple","Banana","Orange",]
+
+
+// for(var i = 0 ; i<=fruits.length ; i++ ){
+
+//     console.log(fruits[i]);
+    
+// }
+
+
+
+// var fruits = ["Mango","Apple","Banana","Orange",]
+
+// fruits.forEach(function(fruit){
+
+// console.log(fruit);
+
+
+
+// })
+
+
+
+// var fruits = ["Mango","Apple","Banana","Orange",]
+
+// for(var i = fruits.length; i>=0 ; i--){
+
+// console.log(fruits[i]);
+
+// }
+
+
+// var fruits = ["Mango","Apple","Banana","Orange",]
+
+
+// for(var i = fruits.length ; i>=0 ; i--){
+
+// console.log(fruits[i]);
+
+// }
+
+
+
+// var fruits = ["Mango","Apple","Banana","Orange",]
+
+// var i = 0;
+
+// while(i<=fruits.length){
+
+
+//     console.log(fruits[i]);
+
+// i++
+    
+// }
+
+
+// var button = document.getElementById("btn");
+
+// button.addEventListener("click", function(){
+
+
+// alert("Button clicked");
+
+
+
+// })
+
+
+
+// var element = document.querySelectorAll(".myClass")
+// console.log(element);
